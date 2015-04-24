c360e201bad4358f764498947d96cb3fa0000fb4
clarejacky <clare.jacky@gmail.com>
2015-04-24 10:56:16 -0500

    more arrays

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var shoutOutArray1 = ["I want to thank the random Nerdery staff that stopped in to sprinkle some knowledge from their mighty brains on us.", "Scott for sitting down with me today. Tracy, Vince for talking to me after I shared my story. They were supportive.", "Tracey for convincing Alicia and i we were wrong about -- and ++ being altered on the global level.", "You all were great. I'm just glad to be here.", "Mary did a great job at catching logical errors going through the challenge today.", "Thanks to Erik!  He taught me how to mirror my displays, great start to the day.", "Every one was super helpful. Aaron and Kaitlin were great partners. Vince was very welcoming to start the first day.", "I am feeling really thankful that everyone on our team is so nice and open.", "Shout out to the guest speaker from the nerdery for doing such a wonderful job! I'm also really appreciate the thoughtful and supportive comments from Clare. She is always super kind and supportive!"];
 
 var shoutOutArray2 = ["Alicia, for helping me to feel a little less overwhelmed.", "I would like to thank Steve Martin and Kaitlin Muth. They both took the time to help me understand how to use indices.", "Everyone! I couldn't ask for a better first day or a better cohort", "I would thank Clare for being my table/row partner and teammate in the challenge, she is very sweet and welcoming. I thank Joseph and Scott and Mark and Fred, all of them contribute greatly to the mood and learning environment in their own ways.  I thank Chelsea and Brian for some moments of comic relief. I thank everyone for being very nice, positive, and friendly.", "Michelle for letting me walk through our challenge and understand it. Casey, Cody, Kaitlin were also big helps. We really worked hard and problem solved like crazy today.", "Yeah, Michael and Clare. You guys were awesome teammates today! We complimented each other very well and were super supportive of one another. You communicated well and helped me to understand things I didn't before. Thanks!", "I want to thank my team, because it felt really good to go back and forth and work together and correct each other, and listen to each other (Brian and Alicia). Also, I want to thank Aaron for driving me to get lunch. It was good to have a conversation with someone about what brought us to this program and our feelings and hopes.", "I would like to thank Jeanne and Chelsea for helping me be more comfortable with the group. Throughout the rest of the day, they would make jokes and laugh with me and that really helped me feel welcome.", "Scott was fantastic about measuring the temperature of the cohort today and having us stop work on the group project. Our group was doing well, but we were definitely at a point of increasing fatigue and diminishing returns. It was really helpful to stop -- we will be more effective and fresh for it today.", "To Brian & Tracy -- Thank you for being constructively supportive during the group assignment. You help me look at things from a different angle and that really made things more clear to me."];
-var shoutOutArray3 = [];
-var shoutOutArray4 = [];
+var shoutOutArray3 = ["Honestly I was so happy to meet everyone today.", "Kaitlin helped me immensely today.", "Thanks to the people who played Resistance with me today! That was fun.", "Way to go, Michelle, Clare and Terry on teamwork today. We may not have won at betting points, but we kicked butt on that challenge we were handed. Thanks, Rom for kicking butt at terminal and git. You're awesome and you've helped me learn a lot about git and javascript--especially with the DOM.", "Shout out to Rom for getting the games rolling during lunch. It was a fun break!", "I want to thank Brian and Rom for getting a game of Resistance going during lunch! It took a little bit of coordination to rope people together and get everyone paying attention to the rules, but it was really fun.", "Today for the peer challenge I was paired with Mary. She was really great to work with. If she knew the answer to something that I didn't, she didn't just tell me the answer, she helped me find it on my own.", "Aaron was a great partner for the pair-programming challenge. We were both very respectful and made sure to help one another get to the answer. It made for a great learning experience.", "Sherman's visit was awesome!", "Thanks to Alicia for her fun attitude and bringing cupcakes. Thanks to Kelly for proposing a board game at lunch. Thanks to Joseph for his help and explanations of the weekend challenge. Thanks to Scott for a great JQuery intro and challenge."];
+var shoutOutArray4 = ["I'd like to give thanks to Tracy! She was already here when I got here at 7 this morning, and it was nice to sit and chat with her for a little bit. We commiserated and helped each other feel better about not understanding the weekend assignment. It was nice to talk w someone that has a very similar way of responding to stress - made me feel less crazy. Also to Terry! He got here around 7:30 this morning and sat with us and helped me understand what I needed to do to get my code to the next step. Thanks to him, I was able to register more than just 1 form input...if that makes sense.", "Rom -- thank you for the '11th hour' save for the weekend challenge! it was BEYOND helpful and made me feel better about what I didn't know and what I have yet to learn. Scott- thank you for being so humble and showing that we all make mistakes and that is okay. We all learned something from this challenge... even if it wasn't a completed project. Michael - Thank you for being so supportive during the peer partnering challenge today. The fact that you take the time to re-go-over things with me to make sure I absorb them is EXTREMELY helpful.", "Really enjoyed David Ly's speech in Toastmasters, thanks to Mary, Terry, Aaron, Brian, Michelle (group members). Thanks to Fred, Joseph, and Scott as always!", "Kaitlin and Rom were extremely helpful in helping us get through the content.", "Scott, for helping us debug our challenge as well as Joseph. Vince and I were an unstoppable force, minus the bug.", "Thanks to Terry and Casey for being great to work with. It's easy to talk over the material with them.", "To Rom & Chelsea: thank you for being awesome and helping Luke and I when we were caught up on function step in our challenge. Both of your inputs were extremely valuable. To Luke: Thank you for being an awesome partner in crime for the pair programming challenge. You helped me through my coding portion and provided support for me.", "Michael was an excellent peer partner. He is patient and thoughtful about explaining things.", "Staff for their humility and understanding of the difficult content.", "I would like to thank my team: Alicia, Steve, and Rom, we worked very well together and Rom especially was an amazing contributor and teacher to all. I was especially impressed when after a brief moment of frustration, our team was able to recognize it and apologize. Everyone was kind and patient and sustained it throughout the rest of the challenge. It was great getting to know them all better. I also thank my partner Clare, we are working very well together, and I thank all the staff again for a great day 2."];
+var shoutOutArray5 = ["My team, Michelle, Kaitlin, and Kc. Really encouraging and supportive/patient with my ?'s and great attitudes working together.", "Scott helped me feel better at the end of the day today. The final solo project was really confusing and I think I let that confusion mess with my head, and that made it impossible for me to look at the code without getting frustrated.Then Scott came over to Aaron and myself and reassured both of us that the frustration we were feeling was normal, and that we are still on-track and doing well.", "Thanks to Vince, Kaitlin, Alicia, and Steve (my team), great working with new people, constantly impressed at how each person contributes something different.", "Erik helped me understand the adding of a space to the array on the solo assignment today. Rom was great with walking around and helping people.", "I want to say thanks to Terry and Scott. Both of them took the time to see how I was doing. Terry was in the group project and kept asking me if I was understanding what was going on.", "I want to give thanks to Clare, Rom, and Kaitlin for being an awesome group team. They helped me to work through what we were trying to do in the project and didn't get mad when I got lost.", "Thanks, Fred. Talking helped.", "Thanks to all my group members (Aaron, Mary, and Terry) for being a patient and easy-going group with each other, and to Rom and Luke for opening my mind to some different ways of structuring the solution to that assignment.", "I saw both Scott and Fred actively working with me and/or others to ensure that we were supported emotionally as well as in the code we were doing. Even as someone who feels comfortable with the coding aspect, it's really helpful to get those additional reassurances that I'm doing well, especially when I feel like I'm about to fall over.", "Thanks to Mark for having a lunch option everyday!"];
+var shoutOutArray6 = ["Thanks to Erik!  He brought me a real mouse to borrow.  Well, not a real real mouse, which would be furry and cute, but a real plastic mouse, which is cute, but not as cute as mini Chewbacca, which is also furry.", "Today was a little rough in the morning. I'd like to thank Brian. He makes me laugh a lot even when I'm in kind of a bad mood, and getting back into the group work with him was fun. I appreciate his company.", "I want to give a big thank you to Casie and Alicia, who were really kind and thoughtful and checked in with me again this morning. Their support made me feel so much better.", "I'm glad that everyone was here today. I know that others are going through some really hard times right now, and I sincerely wish them all the best.", "I want to give thanks to Scott, you're doing a great job despite melting our brains this weekend.", "Thanks to Scott, as usual :-)", "Thanks to everyone who spoke so openly about themselves (Kaitlin, Alicia, Rom, Brian, Chelsea, Clare).", "Thanks to Scott for answer the same questions, over and over, for me today.", "I had an awesome awesome group today. Cody, Kaitlin, Tracy and Kelly were so great to work with.", "Scott, Mark and Fred... thank you guys so much for helping me. Seeing you guys swing into action mode for me was amazing. It is hard to ask for help. Thank you for making it easy."];
+var shoutOutArray7 = [];
+var shoutOutArray8 = [];
+var shoutOutArray9 = [];
+
 var i = 0;
 
 
@@ -19,13 +25,40 @@ $("#shoutOut").on("click", function(){
 	console.log("Hi");
 	for(var i = 0; i < shoutOutArray1.length; i++) {
 		$("#firstDiv").append("<li>"+shoutOutArray1[i]+"</li>");
-		console.log(shoutOutArray1[i]);
 	}
-	$("#shoutOut").fadeOut("slow");
-	$("#shoutOut2").fadeIn();
+	$("#shoutOut").fadeOut();
+	$("#shoutOut2").fadeIn("fast");
 });
 
+$("#shoutOut2").on("click", function(){
+	console.log("Hi");
+	for(var i = 0; i < shoutOutArray2.length; i++) {
+		$("#secondDiv").append("<li>"+shoutOutArray1[i]+"</li>");
+	}
+	$("#shoutOut2").fadeOut("fast");
+	$("#shoutOut3").fadeIn("fast");
+	$("#firstDiv").slideUp("slow");
+});
 
+$("#shoutOut3").on("click", function(){
+	console.log("Hi");
+	for(var i = 0; i < shoutOutArray2.length; i++) {
+		$("#thirdDiv").append("<li>"+shoutOutArray1[i]+"</li>");
+	}
+	$("#shoutOut3").fadeOut("fast");
+	$("#shoutOut4").fadeIn("fast");
+	$("#secondDiv").slideUp("slow");
+});
+
+$("#shoutOut4").on("click", function(){
+	console.log("Hi");
+	for(var i = 0; i < shoutOutArray2.length; i++) {
+		$("#fourthDiv").append("<li>"+shoutOutArray1[i]+"</li>");
+	}
+	$("#shoutOut4").fadeOut("fast");
+	$("#shoutOut5").fadeIn("fast");
+	$("#thirdDiv").slideUp("slow");
+});
 //click button will load shout outs - fade in 
 //button changes to "more shout outs" loads 5 more should outs
 
